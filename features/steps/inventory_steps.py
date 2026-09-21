8e087a7d0ff7d7e13a3c4fd0cb9c9b1574d9e4ce
Vishnu Muruganantham <vishnum30894@gmail.com>
2026-09-21 21:09:58 +0530

    changing the page name accordingly

--- features/steps/inventory_steps.py
+++ features/steps/inventory_steps.py
@@ -1,12 +1,11 @@
-from behave import when, then
-from pages.inventory_page import InventoryPage
+from behave import then
 
 
 @then("the inventory page is displayed")
 def step_inventory(context):
-    assert context.inventory.is_loaded(), "Inventory page did not load"
+    assert context.inventory_page.is_loaded(), "Inventory page did not load"
 
 
 @then("the user should be able to add products to cart")
 def step_open(context):
-    assert context.inventory.select_products(), "Unable to select"
+    assert context.inventory_page.select_products(), "Unable to select"
