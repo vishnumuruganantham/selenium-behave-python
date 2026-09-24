37416fb956f1c688876ec12b35a79327173a9c4f
Vishnu Muruganantham <vishnum30894@gmail.com>
2026-09-24 18:55:41 +0530

    uniform naming of buttons

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -5,10 +5,10 @@ from selenium.webdriver.common.by import By
 class CartPage(BasePage):
 
     title = (By.CSS_SELECTOR, "[data-test='title']")
-    checkout_btn = (By.CSS_SELECTOR, "[data-test='checkout']")
+    btn_checkout = (By.CSS_SELECTOR, "[data-test='checkout']")
 
     def is_loaded(self):
         return self.text(self.title) == "Your Cart"
 
     def navigate_to_checkout_page(self):
-        self.click(self.checkout_btn)
+        self.click(self.btn_checkout)
